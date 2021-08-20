6f30cbadfbe96583dc0701a42a39f79a9a2fb100
Ronnie Rios <ronniesmail14@gmail.com>
2021-08-19 21:15:59 -0500

    added queries to resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,7 +1,43 @@
 const { User, Posts } = require('../models');
 
 const resolvers = {
-    //the routes will go here
+    Query: {
+        me: async(parent, args, context) => {
+            if (context.user) {
+                const userData = await User.findOne({ 
+                    _id: context.user._id
+                })
+                .select('-__v -password')
+                .populate('posts')
+                .populate('comments');
+
+            return userData;
+            }
+        },
+        posts: async(parent, { username }) => {
+            const params = username ? { username } : {};
+            return Posts.find(params).sort({ createdAt: -1 })
+        },
+        //single post
+        post: async (parent, { _id }) => {
+            return Posts.findOne({ _id });
+        },
+        //get all users
+        users: async() => {
+            return User.find()
+            .select('-__v -password')
+            .populate('posts')
+            .populate('comments');
+        },
+        //get by username
+        user: async(parent, { username }) => {
+            return User.findOne({ username })
+            .select('-__v -password')
+            .populate('posts')
+            .populate('comments');
+
+        }
+    }
 }
 
 module.exports = resolvers;
