0963ce371207a25cbc56f0c04794f5a4ee6f90d8
Ronnie Rios <ronniesmail14@gmail.com>
2021-08-21 15:55:42 -0500

    fixed syntax errors in schema typedefs; server now running

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -4,33 +4,34 @@ const { gql } = require("apollo-server-express");
 const typeDefs = gql`
     type User {
         _id: ID,
-        username: String,
+        username: String
         email: String
         post: [Posts]
     }
     type Posts {
         _id: ID,
-        subtitle: String,
-        bodyText: String,
-        createdAt: String,
-        orgLink: String,
+        subtitle: String
+        bodyText: String
+        createdAt: String
+        orgLink: String
         comments: [Comments]
     }
     type Comments {
         _id: ID,
-        commentBody: String,
-        username: String,
-        createdAt: String,
+        commentBody: String
+        username: String
+        createdAt: String
     }
     type Query {
         me: User
-        users: (username: String!): User
+        users: [User]
+        user(username: String!): User
         posts(username: String): [Posts]
         post(_id: ID!): Posts
     }
-    type Mutations {
-        login(username: String!, password: String!)
-        addUser(username: String, email: String!, password: String!)
+    type Mutation {
+        login(username: String!, password: String!): Auth
+        addUser(username: String, email: String!, password: String!): Auth
         addPost(bodyText: String!): Posts
         addComment(postsId: ID!, commentBody: String!): Posts
 
@@ -39,6 +40,6 @@ const typeDefs = gql`
         token: ID!
         user: User
     }
-    `;
+   ` ;
 //export
 module.exports = typeDefs;
