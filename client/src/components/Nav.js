50358fc552d12c0ae0a96d60e220fde74b3bcb2f
Ronnie Rios <ronniesmail14@gmail.com>
2021-08-21 11:28:48 -0500

    nav jsx added

---
+++ client/src/components/Nav.js
@@ -0,0 +1,49 @@
+import React from 'react';
+
+const Nav = (props)=> {
+
+      return (
+        <div>
+          {/* Hello world */}
+          <div className="awesome" style={{border: '1px solid red'}}>
+            <label htmlFor="name">Enter your name  <nav className="navbar navbar-expand-lg navbar-light bg-light sticky-top prelogin-nav">
+                <div className="container-fluid nav-container">
+                  <div className="col-1" />
+                  <div className="col-4">
+                    <a className="navbar-brand mainLogo" href="#"><img src="BU-Logo_Beyond Us.png" className="logoNav" style={{width: '10%'}} /></a>
+                  </div>
+                  <div className="col-2" />
+                  <div className="col-3">
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                      <span className="navbar-toggler-icon" />
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarNavDropdownLeft">
+                      <ul className="navbar-nav">
+                        <li className="nav-item">
+                          <a className="nav-link" aria-current="page" href="#ourmission">Our Mission</a>
+                        </li>
+                        <li className="nav-item">
+                          <a className="nav-link" href="#howtohelp">How to Help</a>
+                        </li>
+                        <li className="nav-item">
+                          <a className="nav-link" href="#jointhecause">Join the Cause</a>
+                        </li>
+                        {/* <li class="nav-item login">
+                    <button type="button" class="btn btn-outline-light logButton btn-sm"><a class="nav-link text-light" href="#">Log In</a></button>
+                  </li> */}
+                      </ul>
+                    </div>
+                  </div>
+                  <div className="col-1"><button type="button" className="btn btn-outline-light logButton btn-sm"><a className="nav-link text-light" href="login.html">Log In</a></button></div>
+                  <div className="col-1" />
+                </div>
+              </nav>: </label>
+            <input type="text" id="name" />
+          </div>
+          <p>Enter your HTML here</p>
+        </div>
+      );
+ };
+
+
+  export default Nav;
