0a137c2606bf0fca0aec41e32ac9296670599d91
rtanguyen <rtanguyen@gmail.com>
2021-08-21 15:00:08 -0500

    landing

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -1,11 +1,18 @@
-import React, { useState } from 'react';
-import { useMutation } from '@apollo/client';
-import { ADD_USER } from '../utils/mutations';
-import Auth from '../utils/auth';
+import React, { useState } from "react";
+import { useMutation } from "@apollo/client";
+import { ADD_USER } from "../utils/mutations";
+import Auth from "../utils/auth";
+import { Link } from "react-router-dom";
 
 const SignupForm = () => {
   // set initial form state
-  const [userFormData, setUserFormData] = useState({ username: '', email: '', password: '' });
+  const [userFormData, setUserFormData] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+
+  const [addUser, { error }] = useMutation(ADD_USER);
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
@@ -17,49 +24,93 @@ const SignupForm = () => {
 
     //use try/catch instead of promises to handle errors
     try {
-      //execute addUser mutation and pass in variable data from form 
+      //execute addUser mutation and pass in variable data from form
       const { data } = await addUser({
-        variables: { ...formState }
+        variables: { ...userFormData },
       });
       console.log(data);
       Auth.login(data.addUser.token);
-    } catch(e) {
+    } catch (e) {
       console.error(e);
     }
   };
 
   return (
-    <div className="col-3">            
+    <div className="col-3">
       <div className="card card-signUp">
         <div className="card-body signUpForm rounded">
-          <p className="card-title text-center fs-4 fw-bold createTitle">Fill out the form below:</p>
+          <p className="card-title text-center fs-4 fw-bold createTitle">
+            Fill out the form below:
+          </p>
           <form className="login-form" onSubmit={handleFormSubmit}>
             <div className="form-label-group">
-              <input type="text" id="username-login" className="form-control" placeholder="Username" required autofocus value={userFormData.username}
-                    onChange={handleInputChange}/>
-              <label htmlFor="username-login" className="fs-6 mt-1">Username</label>
+              <input
+                type="text"
+                id="username-login"
+                className="form-control"
+                placeholder="Username"
+                required
+                autofocus
+                value={userFormData.username}
+                onChange={handleInputChange}
+              />
+              <label htmlFor="username-login" className="fs-6 mt-1">
+                Username
+              </label>
             </div>
             <div className="form-label-group">
-              <input type="text" id="email-login" className="form-control" placeholder="Email Address" required autofocus value={userFormData.email}
-                    onChange={handleInputChange}/>
-              <label htmlFor="username-login" className="fs-6 mt-1">Email Address</label>
+              <input
+                type="text"
+                id="email-login"
+                className="form-control"
+                placeholder="Email Address"
+                required
+                autofocus
+                value={userFormData.email}
+                onChange={handleInputChange}
+              />
+              <label htmlFor="username-login" className="fs-6 mt-1">
+                Email Address
+              </label>
             </div>
             <div className="form-label-group mt-3">
-              <input type="password" id="password-login" className="form-control" placeholder="Password" required value={userFormData.password}
-                    onChange={handleInputChange}/>
-              <label htmlFor="password-login" className="fs-6 mt-1">Password</label>
+              <input
+                type="password"
+                id="password-login"
+                className="form-control"
+                placeholder="Password"
+                required
+                value={userFormData.password}
+                onChange={handleInputChange}
+              />
+              <label htmlFor="password-login" className="fs-6 mt-1">
+                Password
+              </label>
             </div>
             <div className="container btnLogIn">
               <div className="row">
                 <div className="col" />
                 <div className="col d-flex justify-content-end">
-                  <button className="btn btn-sm btn-outline-light logButton signUpCTA" type="submit"><a href="#" className="submitBut nav-link text-light">Submit</a></button>
+                  <button
+                    className="btn btn-sm btn-outline-light logButton signUpCTA"
+                    type="submit"
+                  >
+                    <a href="#" className="submitBut nav-link text-light">
+                      Submit
+                    </a>
+                  </button>
                 </div>
               </div>
             </div>
             <hr className="my-4" />
             <div className="createAccountForm mb-3 d-flex justify-content-end">
-              <p>Already a member?</p><Link to='/login' className='d-block text-center mt-2 small goCreateAccount'>Login</Link>
+              <p>Already a member?</p>
+              <Link
+                to="/login"
+                className="d-block text-center mt-2 small goCreateAccount"
+              >
+                Login
+              </Link>
             </div>
           </form>
         </div>
--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,16 +1,15 @@
-import React from 'react';
-import Auth from '../utils/auth'
-import PrelogHeader from '../components/PrelogHeader';
-import SignupForm from '../Components/SignupForm';
+import React from "react";
+import Auth from "../utils/auth";
+import PrelogHeader from "../components/PrelogHeader";
+import SignupForm from "../components/SignupForm";
 
 const Landing = () => {
-    
-    return (
-        <>
-        <PrelogHeader />
-        <div>
+  return (
+    <>
+      <PrelogHeader />
+      <div>
         {/* LANDING */}
-        <div className="mainJumbo mb-5"> 
+        <div className="mainJumbo mb-5">
           <div className="container jumbo-container">
             <div className="row">
               <div className="col-1" />
@@ -18,16 +17,36 @@ const Landing = () => {
                 <div className="container mt-4">
                   <div className="row">
                     <div className="col-6">
-                      <h1 className="display-3"><span className="jumboHead">Beyond Us</span></h1>
+                      <h1 className="display-3">
+                        <span className="jumboHead">Beyond Us</span>
+                      </h1>
                       <hr className="my-4" />
-                      <p>Connecting remarkable humans to extraordinary organizations with one goal in mind, to help those beyond ourselves. Beyond Us brings forth real world issues that require your help today.
+                      <p>
+                        Connecting remarkable humans to extraordinary
+                        organizations with one goal in mind, to help those
+                        beyond ourselves. Beyond Us brings forth real world
+                        issues that require your help today.
                       </p>
                       <p className="lead">
-                        <button type="button" className="btn btn-danger joinCause btn-med"><a className="nav-link text-light fw-bold" href="#jointhecause">Join the Cause</a></button>
+                        <button
+                          type="button"
+                          className="btn btn-danger joinCause btn-med"
+                        >
+                          <a
+                            className="nav-link text-light fw-bold"
+                            href="#jointhecause"
+                          >
+                            Join the Cause
+                          </a>
+                        </button>
                       </p>
                     </div>
                     <div className="col-4">
-                      <img src="assets/img/savethechildren.jpg" className="img-fluid" alt="Responsive image" />
+                      <img
+                        src="assets/img/savethechildren.jpg"
+                        className="img-fluid"
+                        alt="Responsive image"
+                      />
                     </div>
                   </div>
                 </div>
@@ -50,12 +69,26 @@ const Landing = () => {
             <div className="row">
               <div className="col-2" />
               <div className="col-3">
-                <img src="assets/img/community2.jpg" className="img-fluid" alt="Responsive image" />
+                <img
+                  src="assets/img/community2.jpg"
+                  className="img-fluid"
+                  alt="Responsive image"
+                />
               </div>
               {/* <div class="d-flex align-items-center"> */}
               <div className="col-5 d-flex align-items-center">
                 {/* <h2>Our Mission</h2> */}
-                <p><span className="fs-2">Our Mission</span><br />Climate change, women's rights, poverty, animal cruelty, and more. These issues we face are affecting people around the world and future generations to come if we don't start to make a change. Our platform provides access to hundreds of non-profits and volunteer organizations in defense of the greater good. Together we can make waves that can impact lives and change the world.</p>
+                <p>
+                  <span className="fs-2">Our Mission</span>
+                  <br />
+                  Climate change, women's rights, poverty, animal cruelty, and
+                  more. These issues we face are affecting people around the
+                  world and future generations to come if we don't start to make
+                  a change. Our platform provides access to hundreds of
+                  non-profits and volunteer organizations in defense of the
+                  greater good. Together we can make waves that can impact lives
+                  and change the world.
+                </p>
               </div>
               {/* </div> */}
               <div className="col-1" />
@@ -68,10 +101,20 @@ const Landing = () => {
             <div className="row">
               <div className="col-2" />
               <div className="col-5 d-flex align-items-center">
-                <p><span className="fs-2">How to Help</span><br />Donate. Volunteer. Spread awareness. Be informed. There are many ways to help whether it is big or small. You're making a difference in the world and someone's life.</p>
+                <p>
+                  <span className="fs-2">How to Help</span>
+                  <br />
+                  Donate. Volunteer. Spread awareness. Be informed. There are
+                  many ways to help whether it is big or small. You're making a
+                  difference in the world and someone's life.
+                </p>
               </div>
               <div className="col-3">
-                <img src="assets/img/volunteerhero.jpg" className="img-fluid" alt="Responsive image" />
+                <img
+                  src="assets/img/volunteerhero.jpg"
+                  className="img-fluid"
+                  alt="Responsive image"
+                />
               </div>
               <div className="col-1" />
             </div>
@@ -79,7 +122,9 @@ const Landing = () => {
         </div>
         {/* Landing closing statement */}
         <div className="landClose mb-5">
-          <h1 className="landCloseStatement text-center">Speak up. Reach out. Make an impact.</h1>
+          <h1 className="landCloseStatement text-center">
+            Speak up. Reach out. Make an impact.
+          </h1>
         </div>
         {/* Discover causes */}
         <div className="discover mb-5 mt-5">
@@ -87,7 +132,11 @@ const Landing = () => {
             <div className="row">
               <div className="col-2" />
               <div className="col-8">
-                <div id="carouselExampleCaptions" className="carousel slide" data-bs-ride="carousel">
+                <div
+                  id="carouselExampleCaptions"
+                  className="carousel slide"
+                  data-bs-ride="carousel"
+                >
                   {/* <div class="carousel-indicators">
                 <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
                 <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
@@ -95,30 +144,58 @@ const Landing = () => {
               </div> */}
                   <div className="carousel-inner">
                     <div className="carousel-item active">
-                      <img src="./assets/img/animalrights1.jpg" className="d-block w-100" alt="..." />
+                      <img
+                        src="./assets/img/animalrights1.jpg"
+                        className="d-block w-100"
+                        alt="..."
+                      />
                       <div className="carousel-caption d-none d-md-block">
                         <h5>Protect Animals lives</h5>
                       </div>
                     </div>
                     <div className="carousel-item">
-                      <img src="./assets/img/protectafgans2.jpg" className="d-block w-100" alt="..." />
+                      <img
+                        src="./assets/img/protectafgans2.jpg"
+                        className="d-block w-100"
+                        alt="..."
+                      />
                       <div className="carousel-caption d-none d-md-block">
                         <h5>Help Save Afghans</h5>
                       </div>
                     </div>
                     <div className="carousel-item">
-                      <img src="./assets/img/wilefire.jpg" className="d-block w-100" alt="..." />
+                      <img
+                        src="./assets/img/wilefire.jpg"
+                        className="d-block w-100"
+                        alt="..."
+                      />
                       <div className="carousel-caption d-none d-md-block">
                         <h5>Preserve our Planet</h5>
                       </div>
                     </div>
                   </div>
-                  <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
-                    <span className="carousel-control-prev-icon" aria-hidden="true" />
+                  <button
+                    className="carousel-control-prev"
+                    type="button"
+                    data-bs-target="#carouselExampleCaptions"
+                    data-bs-slide="prev"
+                  >
+                    <span
+                      className="carousel-control-prev-icon"
+                      aria-hidden="true"
+                    />
                     <span className="visually-hidden">Previous</span>
                   </button>
-                  <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
-                    <span className="carousel-control-next-icon" aria-hidden="true" />
+                  <button
+                    className="carousel-control-next"
+                    type="button"
+                    data-bs-target="#carouselExampleCaptions"
+                    data-bs-slide="next"
+                  >
+                    <span
+                      className="carousel-control-next-icon"
+                      aria-hidden="true"
+                    />
                     <span className="visually-hidden">Next</span>
                   </button>
                 </div>
@@ -130,9 +207,7 @@ const Landing = () => {
       </div>
       <SignupForm />
     </>
-    )
-}; 
-
-
+  );
+};
 
 export default Landing;
