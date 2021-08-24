0e5b01685e596a7dc6e0f9741820ea8eb2e8cc0d
Mimi Vo <mimzy414@gmail.com>
2021-08-23 22:29:49 -0500

    comment form fix

--- client/src/components/CommentForm.js
+++ client/src/components/CommentForm.js
@@ -2,22 +2,32 @@ import React from 'react'
 
 const CommentForm = () => {
    return(
+
       <div className="container mb-5">
-        <h4>
-          See what people are saying about this issue and share your thoughts below.
+        <hr />
+        <h4 className="pt-4">
+          Comments
         </h4>
-        <div className="mb-3 commentBox">
-          <label htmlFor="exampleFormControlTextarea1" className="form-label" />
-          <textarea className="form-control" id="exampleFormControlTextarea1" rows={3} defaultValue={""} />
+        <div className="row">
+          {/* <div class="col-1"></div> */}
+          <div className="mb-3 commentBox col-10">
+            <label htmlFor="exampleFormControlTextarea1" className="form-label" />
+            <textarea className="form-control" id="exampleFormControlTextarea1" rows={3} defaultValue={""} />
+          </div>
+          <div className="col-2" />
         </div>
         <div className="postedComments container">
           <div className="row pastComments" id="commentPast">
             <div className="col-1" />
-            <div className="col-3">
-              <p className="name">Beyonce</p>
-              <p className="date">12.23.2013</p>
+            <div className="container">
+              <div className="row">
+                <div className="col-2">
+                  <p className="name fw-bold">Beyonce</p>
+                  <p className="date">12.23.2013</p>
+                </div>
+                <div className="col-7">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatibus sunt rem nihil recusandae? Tenetur tempora temporibus nam, dolor eius eaque, sint, totam vitae culpa voluptate accusamus minus harum! At, qui.</div>
+              </div>
             </div>
-            <div className="col-7">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatibus sunt rem nihil recusandae? Tenetur tempora temporibus nam, dolor eius eaque, sint, totam vitae culpa voluptate accusamus minus harum! At, qui.</div>
             <div className="col-1" />
           </div>
         </div>
