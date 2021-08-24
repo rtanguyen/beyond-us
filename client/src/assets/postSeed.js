83baeff180ba85bd3346ff3bd70ac37e356760b9
rtanguyen <rtanguyen@gmail.com>
2021-08-23 20:15:24 -0500

    seed

---
+++ client/src/assets/postSeed.js
@@ -0,0 +1,50 @@
+import afghan1Img from "../assets/protextafgans.jpg";
+import covidImg1 from "../assets/covid3.jpg";
+import haitiImg from "../assets/haiti1.jpg";
+import climateChangeImg from "../assets/climatechange3.jpg";
+import afghanImg2 from "../assets/afghan.jpg";
+import covidImg2 from "../assets/covid2.jpg";
+import haitiImg3 from "../assets/haiti4.jpg";
+
+const postSeed = [
+  {
+    title: "Help save Afghans",
+    subtitle:
+      "As the Taliban has breached the capital city of Kabul, thousands of Afghans are fighting to find a way out. Humanitarians and soldiers on the ground are doing what they can, but here is our oppurtunity to help save hundreds of lives. Read more on the efforts to get planes on the ground to bring Afghans to safety.",
+    bodyText:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. ",
+    createdAt: new Date(Date.now()),
+    image: afghan1Img,
+    orgLink: "",
+  },
+  {
+    title: "COVID-19: Global Pandemic",
+    subtitle:
+      " As we battle our everyday stresses, we are also at waar with a contagion that has sweep across the globe. Medical workers are working endless hours to help protect us. Here's how we can help prevent the spread.",
+    bodyText:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    createdAt: new Date(Date.now()),
+    image: covidImg1,
+    orgLink: "",
+  },
+  {
+    title: "Catostophic earthquake struck Haiti",
+    subtitle:
+      "Haiti was recently hit by a 7.2 magnitude earthquake leaving over a thousand Haitians and counting dead. As rescue efforts are in progress, survivors, many left without homes and any possesions, are in search of aid, food, and supplies.",
+    bodyText:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    createdAt: new Date(Date.now()),
+    image: haitiImg,
+    orgLink: "",
+  },
+  // {
+  //   title: "",
+  //   subtitle: "",
+  //   bodyText: "",
+  //   createdAt: new Date(Date.now()),
+  //   image: "",
+  //   orgLink: "",
+  // },
+];
+
+export default postSeed;
--- client/src/utils/auth.js
+++ client/src/utils/auth.js
@@ -35,7 +35,7 @@ class AuthService {
   login(idToken) {
     localStorage.setItem("id_token", idToken);
 
-    // window.location.assign("/dashboard");
+    window.location.assign("/home");
   }
 
   //clear token from localStorage and force logout with reload
