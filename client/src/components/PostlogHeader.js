4ec5fc5213dfcca78ea04a7ba04c7583521b0afd
rtanguyen <rtanguyen@gmail.com>
2021-08-25 13:43:35 -0500

    conditional render add post page

--- client/src/components/PostlogHeader.js
+++ client/src/components/PostlogHeader.js
@@ -6,46 +6,68 @@ import { Link } from "react-router-dom";
 const PostlogHeader = () => {
   return (
     <header className="header">
-        {/* NAVIGATION */}
-        <nav className="navbar navbar-expand-lg navbar-light bg-light sticky-top prelogin-nav px-5">
-          <div className="container-fluid nav-container ">
-            <Link to={"/"}>
-                <img src={LogoPic} className="logoNav navbar-brand mainLogo" style={{ width: "13%" }} />
-              </Link>
-            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-              <span className="navbar-toggler-icon" />
-              <span />
-              <span />
-            </button>
-            <div className="collapse navbar-collapse justify-content-between" id="navbarSupportedContent">
-              <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-                <li className="nav-item">
-                    <Link to={`/home`} className="nav-link">
-                        Happening Now
-                      </Link>
-                </li>
-                <li className="nav-item">
-                    <Link to={`/home`} className="nav-link">
-                        Trending
-                      </Link>
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link" href="#news">
-                        News
-                      </a>
-                </li>
-                <li className="nav-item">
-                    <Link to={`/add`} className="nav-link">
-                        Create
-                      </Link>
-                </li>
-                <button type="button" className="btn btn-outline-light logButton btn-sm login ms-2"><Link to="/" className="nav-link text-light">Log out</Link></button>
-              </ul>
-            </div>
+      {/* NAVIGATION */}
+      <nav className="navbar navbar-expand-lg navbar-light bg-light sticky-top prelogin-nav px-5">
+        <div className="container-fluid nav-container ">
+          <Link to={"/home"}>
+            <img
+              src={LogoPic}
+              className="logoNav navbar-brand mainLogo"
+              style={{ width: "13%" }}
+            />
+          </Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon" />
+            <span />
+            <span />
+          </button>
+          <div
+            className="collapse navbar-collapse justify-content-between"
+            id="navbarSupportedContent"
+          >
+            <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <Link to={`/home`} className="nav-link">
+                  Happening Now
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link to={`/home`} className="nav-link">
+                  Trending
+                </Link>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#news">
+                  News
+                </a>
+              </li>
+              <li className="nav-item">
+                <Link to={`/add`} className="nav-link">
+                  Create
+                </Link>
+              </li>
+              <button
+                type="button"
+                className="btn btn-outline-light logButton btn-sm login ms-2"
+              >
+                <Link to="/" className="nav-link text-light">
+                  Log out
+                </Link>
+              </button>
+            </ul>
           </div>
-        </nav>
-      </header>
-    );
-  };
+        </div>
+      </nav>
+    </header>
+  );
+};
 
 export default PostlogHeader;
