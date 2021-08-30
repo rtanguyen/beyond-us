bb9884fe40fecd31f340b0058a7c1d96aaf84e83
rtanguyen <rtanguyen@gmail.com>
2021-08-29 22:20:40 -0500

    login button

--- client/src/pages/LoginForm.js
+++ client/src/pages/LoginForm.js
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 import Auth from "../utils/auth";
 import { LOGIN_USER } from "../utils/mutations";
 import { HashLink } from "react-router-hash-link";
-import PrelogHeader from "../components/PrelogHeader"
+import PrelogHeader from "../components/PrelogHeader";
 
 const LoginForm = (props) => {
   const [userFormData, setUserFormData] = useState({
@@ -29,7 +29,7 @@ const LoginForm = (props) => {
     event.preventDefault();
     console.log(userFormData);
     try {
-      const {data} = await login({
+      const { data } = await login({
         variables: { ...userFormData },
       });
       console.log(data);
@@ -49,77 +49,77 @@ const LoginForm = (props) => {
 
   return (
     <>
-    <PrelogHeader/>
-    <div className="container login">
-      <div className="row">
-        <div className="col-md-4" />
-        <div className="col-md-4">
-          <div className="card card-signin my-5">
-            <div className="card-body loginForm">
-              <h5 className="card-title text-center fs-2 fw-bold">Sign in</h5>
-              <form className="login-form" onSubmit={handleFormSubmit}>
-                <div className="form-label-group">
-                  <input
-                    type="text"
-                    id="username-login"
-                    className="form-control"
-                    placeholder="Username"
-                    defaultValue={username}
-                    name="username"
-                    onChange={handleInputChange}
-                    required
-                    autofocus
-                  />
-                  <label htmlFor="username-login" className="fs-6 mt-1">
-                    Username
-                  </label>
-                </div>
-                <div className="form-label-group mt-3">
-                  <input
-                    type="password"
-                    id="password-login"
-                    className="form-control"
-                    placeholder="Password"
-                    required
-                    name="password"
-                    defaultValue={password}
-                    onChange={handleInputChange}
-                  />
-                  <label htmlFor="password-login" className="fs-6 mt-1">
-                    Password
-                  </label>
-                </div>
-                {error && <div>Login failed</div>}
+      <PrelogHeader />
+      <div className="container login">
+        <div className="row">
+          <div className="col-md-4" />
+          <div className="col-md-4">
+            <div className="card card-signin my-5">
+              <div className="card-body loginForm">
+                <h5 className="card-title text-center fs-2 fw-bold">Sign in</h5>
+                <form className="login-form" onSubmit={handleFormSubmit}>
+                  <div className="form-label-group">
+                    <input
+                      type="text"
+                      id="username-login"
+                      className="form-control"
+                      placeholder="Username"
+                      defaultValue={username}
+                      name="username"
+                      onChange={handleInputChange}
+                      required
+                      autofocus
+                    />
+                    <label htmlFor="username-login" className="fs-6 mt-1">
+                      Username
+                    </label>
+                  </div>
+                  <div className="form-label-group mt-3">
+                    <input
+                      type="password"
+                      id="password-login"
+                      className="form-control"
+                      placeholder="Password"
+                      required
+                      name="password"
+                      defaultValue={password}
+                      onChange={handleInputChange}
+                    />
+                    <label htmlFor="password-login" className="fs-6 mt-1">
+                      Password
+                    </label>
+                  </div>
+                  {error && <div>Login failed</div>}
 
-                <div className="container btnLogIn">
-                  <div className="row">
-                    <div className="col" />
-                    <div className="col d-flex justify-content-end">
-                      <button
-                        className="btn btn btn-outline-dark btn-med signInCTA"
-                        type="submit"
-                      >
-                        Log in
-                        <Link to="/home">Login</Link>
-                      </button>
+                  <div className="container btnLogIn">
+                    <div className="row">
+                      <div className="col" />
+                      <div className="col d-flex justify-content-end">
+                        <button
+                          className="btn btn btn-outline-dark btn-med signInCTA"
+                          type="submit"
+                        >
+                          Log in
+                          <Link to="/home"></Link>
+                        </button>
+                      </div>
                     </div>
                   </div>
-                </div>
-                <hr className="my-4" />
-                <div className="loginCreateAccount mb-3 d-flex justify-content-end">
-                  <p>New around here? </p>
-                  {/* <Link to="/">Create your account</Link> */}
-                  <HashLink smooth to="/#jointhecause">
-                     Create your account
-                  </HashLink>
-                </div>
-              </form>
+                  <hr className="my-4" />
+                  <div className="loginCreateAccount mb-3 d-flex justify-content-end">
+                    <p>New around here? </p>
+                    {/* <Link to="/">Create your account</Link> */}
+                    <HashLink smooth to="/#jointhecause">
+                      Create your account
+                    </HashLink>
+                  </div>
+                </form>
+              </div>
             </div>
           </div>
+          <div className="col-md-4" />
         </div>
-        <div className="col-md-4" />
       </div>
-    </div>
     </>
   );
 };
